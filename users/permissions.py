7fd32a9c13c48f17387e6d97cee12a60c152c4f1
Agahan14 <Imamidinov.agahan06@gmail.com>
2022-04-15 23:44:24 +0600

    changin permissions

--- users/permissions.py
+++ users/permissions.py
@@ -9,7 +9,7 @@ class IsAdminOrReadOnly(BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return bool(request.user and request.user.is_staff)
+        return bool(request.user and request.user.is_superuser)
 
 
 class IsAuthenticated(permissions.BasePermission):
